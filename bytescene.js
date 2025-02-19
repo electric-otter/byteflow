c92b30082b7f05a256985dbc399e15d45df4349e
electric-otter <electric-otter-github@proton.me>
2025-02-19 09:05:06 -0500

    Create bytescene.js

---
+++ bytescene.js
@@ -0,0 +1,20 @@
+function executeUserCode() {
+    const code = document.getElementById('userInput').value; // Capture the user input
+
+    try {
+        // Execute the user-provided code
+        eval(code);
+    } catch (error) {
+        console.error("Invalid word:", error);
+    }
+}
+function math(variable)
+  Math.ceil(4.9);
+function rounds()
+  Math.round(4.6);
+function and(function)
+  return function + function
+function say(input)
+  const outputElement =
+document.getElementById('output');
+  outputElement.TextContent = text
